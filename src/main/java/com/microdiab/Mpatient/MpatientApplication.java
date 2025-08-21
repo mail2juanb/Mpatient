b04647a79412ea71173875ae2764fc7b87df0a1c
juanb <mail2juanb@yahoo.fr>
2025-08-21 20:33:57 +0200

    Disable security

--- src/main/java/com/microdiab/Mpatient/MpatientApplication.java
+++ src/main/java/com/microdiab/Mpatient/MpatientApplication.java
@@ -2,8 +2,10 @@ package com.microdiab.Mpatient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MpatientApplication {
 
     public static void main(String[] args) {
