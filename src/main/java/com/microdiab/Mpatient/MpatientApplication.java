f5385fb3fd539453db7e1402b6f4e64bec7202e2
juanb <mail2juanb@yahoo.fr>
2025-08-21 20:25:07 +0200

    Reindent files with 4 spaces

--- src/main/java/com/microdiab/Mpatient/MpatientApplication.java
+++ src/main/java/com/microdiab/Mpatient/MpatientApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MpatientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MpatientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MpatientApplication.class, args);
+    }
 
 }
--- src/test/java/com/microdiab/Mpatient/MpatientApplicationTests.java
+++ src/test/java/com/microdiab/Mpatient/MpatientApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MpatientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
