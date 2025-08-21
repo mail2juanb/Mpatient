973fae0fef2d5a27a49d63d7f1113cf2bd669cd6
juanb <mail2juanb@yahoo.fr>
2025-08-21 20:20:30 +0200

    Initial commit

---
+++ src/main/java/com/microdiab/Mpatient/MpatientApplication.java
@@ -0,0 +1,13 @@
+package com.microdiab.Mpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpatientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microdiab/Mpatient/MpatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microdiab.Mpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
