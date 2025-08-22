c7c6a7500421f94745c44c85f360680739a3b195
juanb <mail2juanb@yahoo.fr>
2025-08-22 10:51:02 +0200

    Add schema and data ressources

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS patient;
+
+CREATE TABLE IF NOT EXISTS patient (
+    id                  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    lastname            VARCHAR(100) NOT NULL,
+    firstname           VARCHAR(100) NOT NULL,
+    dateofbirth         DATE,
+    gender              VARCHAR(1),
+    address             VARCHAR(255),
+    phone               VARCHAR(50)
+);
