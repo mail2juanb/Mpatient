949a4a0d516386bfcb3466cacd4657bfafe5cf5d
juanb <mail2juanb@yahoo.fr>
2025-09-01 11:08:03 +0200

    - Change spring application name for mpatient. - Restaure complete bootstrap.properties for optional local config. - Add Eureka client config

--- src/main/java/com/microdiab/Mpatient/MpatientApplication.java
+++ src/main/java/com/microdiab/Mpatient/MpatientApplication.java
@@ -3,9 +3,11 @@ package com.microdiab.Mpatient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableDiscoveryClient
 public class MpatientApplication {
 
     public static void main(String[] args) {
